f2dccffc95d488a79afdb9dfda6d893d2e8b14af
Christian S <Saregadon@gmail.com>
2021-03-30 23:28:13 -0500

    Fixing matplotlib and adding tk to see visualgraph

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-#python 3.7 -m pip install numpy scipy matplotlib
+#python3.7 -m pip install numpy scipy matplotlib pandas scikit-learn
+#sudo apt-get install python3-tk inside of linux Terminal, not python environment
+import sklearn as skl
+from sklearn import datasets
+from sklearn.model_selection import cross_val_predict
+from sklearn import linear_model
+import matplotlib.pyplot as mp
 import numpy as np
 import scipy as sp
 import pandas as pd
-import matplotlib as mp
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import sklearn as skl
 
+#inpfile = pd.read_csv(r'')
 
+lr = linear_model.LinearRegression()
+boston = datasets.load_boston()
+y = boston.target
+
+predicted = cross_val_predict(lr, boston.data, y, cv = 10)
+
+fig, ax = plt.subplots()
+ax.scatter(y,predicted)
+ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+plt.show()
 
 print("done")
