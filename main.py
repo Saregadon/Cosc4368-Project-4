0fa0534b407b67a7522fd79ed79d600d27e6563a
Christian S <Saregadon@gmail.com>
2021-03-31 01:20:12 -0500

    possibly understand where I should go

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 #sudo apt-get install python3-tk inside of linux Terminal, not python environment
 import csv
 import sklearn as skl
+import seaborn as sms
+from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 from sklearn import datasets
-from sklearn.model_selection import cross_val_predict
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_predict, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
-#from sklearn.externals import joblib
 import matplotlib.pyplot as mp
 import numpy as np
 import scipy as sp
@@ -18,30 +20,21 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 #must read in entire csv file path
-inputfile = pd.read_csv("/home/useradd/Eick-4368-AI-Code/Cosc4368/Cosc4368-AI-Assignment-2/Cosc4368-Project-4/hearts.csv", header = 0)
+data = pd.read_csv("/home/useradd/Eick-4368-AI-Code/Cosc4368/Cosc4368-AI-Assignment-2/Cosc4368-Project-4/hearts.csv", header = 0)
 #print(inputfile.head())
 
-print(inputfile)
+print(data) #prints all data
+print(data.head()) #prints first 5 sets #from pandas
+print(data.info()) #prints the data types of our set
 
-"""
-dataset = pd.DataFrame()
 
-lr = linear_model.LinearRegression()
-SVM = inputfile
-y = SVM.target
 
-#predicted has cross validation of 10 here -- cv == 10
-predicted = cross_val_predict(lr, SVM.data, y, cv = 10)
-
-fig, ax = plt.subplots()
-ax.scatter(y,predicted)
-
-#
-ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-
-ax.set_xlabel('Failure Rates')
-ax.set_ylabel('Heart Rates')
-plt.show()
-"""
+#print(data.isnull().sum()) #gives a summation of how many null values are in each one
+#obviously for this, there will be 0 non-null values
 
 print("done")
+
+#Concentration is to attempt to show the presence of heart disease in patients, starting from 0(absent) to 4 -- [0, 1, 2, 3, 4]
+#possibly call data in 4 times
+#have data used to run for Linear Regression, Decision Tree?
+#then data used for Logistic/sigmoid function, Tanh, or Relu
