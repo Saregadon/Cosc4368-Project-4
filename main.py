d9c564ab25c9d0ece533450ead0e51e85d328a96
Christian S <Saregadon@gmail.com>
2021-03-31 00:32:47 -0500

    updated output

--- main.py
+++ main.py
@@ -2,30 +2,36 @@
 #sudo apt-get install python3-tk inside of linux Terminal, not python environment
 import csv
 import sklearn as skl
+from sklearn.neural_network import MLPClassifier
 from sklearn import datasets
 from sklearn.model_selection import cross_val_predict
+from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
+#from sklearn.externals import joblib
 import matplotlib.pyplot as mp
 import numpy as np
 import scipy as sp
 import pandas as pd
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import sklearn as skl
 
-ifs = "/home/useradd/Eick-4368-AI-Code/Cosc4368/Cosc4368-AI-Assignment-2/Cosc4368-Project-4/hearts.csv"
+#must read in entire csv file path
+inputfile = pd.read_csv("/home/useradd/Eick-4368-AI-Code/Cosc4368/Cosc4368-AI-Assignment-2/Cosc4368-Project-4/hearts.csv", header = 0)
+#print(inputfile.head())
 
-inputfile = pd.read_csv(ifs, header = 0)
+print(inputfile)
 
+"""
 dataset = pd.DataFrame()
 
 lr = linear_model.LinearRegression()
-boston = datasets.load_boston()
-y = boston.target
+SVM = inputfile
+y = SVM.target
 
 #predicted has cross validation of 10 here -- cv == 10
-predicted = cross_val_predict(lr, boston.data, y, cv = 10)
+predicted = cross_val_predict(lr, SVM.data, y, cv = 10)
 
 fig, ax = plt.subplots()
 ax.scatter(y,predicted)
@@ -33,8 +39,9 @@ ax.scatter(y,predicted)
 #
 ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
 
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
+ax.set_xlabel('Failure Rates')
+ax.set_ylabel('Heart Rates')
 plt.show()
+"""
 
 print("done")
